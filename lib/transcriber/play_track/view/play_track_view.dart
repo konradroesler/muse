b9d2dbf8ac0e1ea9eabe3ddc7ff80e1a32532ced
konradroesler <konrad.rosler@gmail.com>
2024-08-25 16:40:47 +0200

    basic PlayTrackPage

--- lib/transcriber/play_track/view/play_track_view.dart
+++ lib/transcriber/play_track/view/play_track_view.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../bloc/play_track_bloc.dart';
 
 class PlayTrackView extends StatelessWidget {
   const PlayTrackView({super.key});
@@ -7,10 +10,31 @@ class PlayTrackView extends StatelessWidget {
   Widget build(BuildContext context) {
     // TODO implement slider and play/stop button
     return Scaffold(
-      body: const Center(
-        child: Text(
-          'Play Track', style: TextStyle(fontSize: 24)
-        )
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            SizedBox(
+              width: 300,
+              child: Text(
+                context.read<PlayTrackBloc>().track.name,
+                style: const TextStyle(fontSize: 16),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            const SizedBox(height: 16),
+            FloatingActionButton(
+              onPressed: () {
+                // TODO toggle audio player
+              },
+              child: Icon(
+                // TODO change icon based on playing status
+                Icons.play_arrow
+              ),
+            ),
+          ],
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
